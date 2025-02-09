05dacffb40b36920d944160bb52a53005a33db30
Keoki Stevenson <keokisteveson@gmail.com>
2025-02-09 11:09:19 -0500

    Add DiscountsController and Views

---
+++ Controllers/DiscountsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BeSpokedBikes.Data;
+using BeSpokedBikes.Models;
+
+namespace BeSpokedBikes.Controllers
+{
+    public class DiscountsController : Controller
+    {
+        private readonly BeSpokedContext _context;
+
+        public DiscountsController(BeSpokedContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Discounts
+        public async Task<IActionResult> Index()
+        {
+            var beSpokedContext = _context.Discounts.Include(d => d.Product);
+            return View(await beSpokedContext.ToListAsync());
+        }
+
+        // GET: Discounts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var discount = await _context.Discounts
+                .Include(d => d.Product)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            return View(discount);
+        }
+
+        // GET: Discounts/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Manufacturer");
+            return View();
+        }
+
+        // POST: Discounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProductId,BeginDate,EndDate,DiscountPercentage")] Discount discount)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(discount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Manufacturer", discount.ProductId);
+            return View(discount);
+        }
+
+        // GET: Discounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var discount = await _context.Discounts.FindAsync(id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Manufacturer", discount.ProductId);
+            return View(discount);
+        }
+
+        // POST: Discounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProductId,BeginDate,EndDate,DiscountPercentage")] Discount discount)
+        {
+            if (id != discount.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(discount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DiscountExists(discount.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Manufacturer", discount.ProductId);
+            return View(discount);
+        }
+
+        // GET: Discounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var discount = await _context.Discounts
+                .Include(d => d.Product)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            return View(discount);
+        }
+
+        // POST: Discounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var discount = await _context.Discounts.FindAsync(id);
+            if (discount != null)
+            {
+                _context.Discounts.Remove(discount);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DiscountExists(int id)
+        {
+            return _context.Discounts.Any(e => e.Id == id);
+        }
+    }
+}
