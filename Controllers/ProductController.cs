23e05a89de55a1a3cff48cde29c43bd8d2e17dc4
Keoki Stevenson <keokisteveson@gmail.com>
2025-02-09 10:41:29 -0500

    Add Product Controller and View

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BeSpokedBikes.Models;
+using Microsoft.EntityFrameworkCore;
+using BeSpokedBikes.Data;
+
+namespace BeSpokedBikes.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly BeSpokedContext _context;
+
+        public ProductController(BeSpokedContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> products =  _context.Products.ToList();
+            return View(products);
+        }
+    }
+}
+
